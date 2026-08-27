6f38d8f2d6474b5d31c2f77769b9c187a54ffac4
pank-hub <kafley@outlook.com>
2026-08-27 21:01:38 +0000

    add social sharing metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import { loadEnv } from "vite";
 const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
 
 export default defineConfig({
+  site: "https://nepali.no",
+
   integrations: [
     sanity({
       projectId: env.PUBLIC_SANITY_PROJECT_ID,
